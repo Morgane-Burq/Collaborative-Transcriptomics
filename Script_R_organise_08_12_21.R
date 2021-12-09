64a7c76ffedb5a1e0d194236b74cf4279819c729
NicoSalas34 <nicolas.salas@supagro.fr>
2021-12-09 08:02:28 +0100

    09/12/21_2

--- Script_R_organise_08_12_21.R
+++ Script_R_organise_08_12_21.R
@@ -10,6 +10,17 @@ library(cowplot)
 
 liste_rht<-read.csv("liste_gÈnotypes.csv")
 data_final<-as_tibble(read.csv("Mesures_neighbors_final.tsv",header=TRUE,sep="\t"))
+data_final_pair = data_final[c(1:270)*2,]
+data_final_impair = data_final[c((1:270)*2 -1),]
+data_final_pair$voisin = data_final_impair$plante_genotype
+data_final_impair$voisin = data_final_pair$plante_genotype
+data_final = as.data.frame(rbind(data_final_pair,data_final_impair))
+data_final<-data_final[-c(16)]
+data_final$Ligne = as.numeric(data_final$Ligne)
+data_final$Colonne = as.numeric(data_final$Colonne)
+data_final[data_final$Bloc %in% c(2,4,6),]$Ligne = data_final[data_final$Bloc %in% c(2,4,6),]$Ligne + 9
+data_final[data_final$Bloc %in% c(5,6),]$Colonne = data_final[data_final$Bloc %in% c(5,6),]$Colonne + 10
+data_final[data_final$Bloc %in% c(3,4),]$Colonne = data_final[data_final$Bloc %in% c(3,4),]$Colonne + 5
 for (elt in (1:nrow(data_final))){
   if (data_final$plante_genotype[elt]%in%c("114","235","329","376","395","482","89")){
     data_final$RHT_rht[elt]="rht"
@@ -27,17 +38,28 @@ liste2<-which(is.na(data_final$Longueur_feuille_2))
 liste3<-which(data_final$Couple%in%data_final$Couple[liste2])
 liste_vitesse_coleoptile_feuille_2<-append(liste3,which(data_final$Couple == data_final$Couple[which(data_final$vitesse_coleoptile_feuille_2<0)]))
 
-data_final<-data_final[-liste3,] %>% 
-  mutate(plante_genotype=as.character(plante_genotype),
-         RHT_rht=as.character(RHT_rht),
+data_final<-data_final %>% 
+  mutate(plante_genotype=as.factor(plante_genotype),
+         RHT_rht=as.factor(RHT_rht),
          Nombre_de_feuille_ligul√.e=as.numeric(Nombre_de_feuille_ligul√.e),
          Longueur_feuille_2=as.numeric(Longueur_feuille_2),
          vitesse_longueur_f1=as.numeric(vitesse_longueur_f1),
          vitesse_largeur_f1=as.numeric(vitesse_largeur_f1),
          poids_graine=as.numeric(poids_graine),
          vitesse_coleoptile_feuille_1=as.numeric(vitesse_coleoptile_feuille_1),
-         vitesse_coleoptile_feuille_2=as.numeric(vitesse_coleoptile_feuille_2)) %>% 
-  filter(Longueur_feuille_2>50)
+         vitesse_coleoptile_feuille_2=as.numeric(vitesse_coleoptile_feuille_2),
+         Monoculture_Couple=as.factor(Monoculture_Couple),
+         Condition_hydrique=as.factor(Condition_hydrique),
+         RHT_rht=as.factor(RHT_rht),
+         Bloc=as.factor(Bloc),
+         Colonne=as.factor(Colonne),
+         Ligne=as.factor(Ligne),
+         plante_genotype=as.factor(plante_genotype),
+         Nombre_de_feuille_ligul√.e=as.numeric(Nombre_de_feuille_ligul√.e),
+         Couple=as.factor(Couple),
+         Position=as.factor(Position),
+         Position_pots=as.factor(Position_pots)) 
+data_final<-data_final[-liste3,]
 
 data_vitesse_coleoptile_feuille_2<-data_final %>% 
   filter(vitesse_coleoptile_feuille_2>0)
@@ -77,7 +99,6 @@ hist(data_vitesse_largeur_f1$vitesse_largeur_f1)
 hist(data_dates$date_ligule_2)
 hist(data_final$Biomasse_seche)
 shapiro.test(data_final$Biomasse_seche)
-
 data<-data_final %>% 
   select(poids_graine,plante_genotype) %>% 
   group_by(plante_genotype) %>% 
@@ -118,20 +139,22 @@ summary(lm_date_ligule_f2_6)
 anova(lm_date_ligule_f2_6)
 ##7
 
-SG_num
 
 data_final$Ligne = as.numeric(data_final$Ligne)
-data_final[data_final$Bloc %in% c(2,4,6),]$Ligne = data_final[data_final$Bloc %in% c(2,4,6),]$Ligne + 100
-
 data_final$Colonne = as.numeric(data_final$Colonne)
-data_final[data_final$Bloc %in% c(1,2),]$Colonne = data_final[data_final$Bloc %in% c(1,2),]$Colonne + 100
-data_final[data_final$Bloc %in% c(3,4),]$Colonne = data_final[data_final$Bloc %in% c(3,4),]$Colonne + 200
+data_final[data_final$Bloc %in% c(2,4,6),]$Ligne = data_final[data_final$Bloc %in% c(2,4,6),]$Ligne + 9
+data_final[data_final$Bloc %in% c(5,6),]$Colonne = data_final[data_final$Bloc %in% c(5,6),]$Colonne + 10
+data_final[data_final$Bloc %in% c(3,4),]$Colonne = data_final[data_final$Bloc %in% c(3,4),]$Colonne + 5
 
 data_final$Bloc = as.factor(data_final$Bloc)
-mod = lm(Biomasse_seche ~ Bloc + Ligne, data = data_final)
-mod = lm(Biomasse_seche ~ Bloc + Ligne + poids_graine+ plante_genotype , data = data_final)
+mod = lm(Biomasse_seche ~ Bloc + poids_graine + plante_genotype, data = data_final)
+mod1 = lm(Biomasse_seche ~ Bloc + poids_graine + plante_genotype:voisin, data = data_final)
+summary(mod)
 anova(mod)
 
+write_csv(data_final,"Mesure_Neighbors_Arrange.csv")
+
+?write_csv
 ?as.numeric
 ?as.numeric
 ?as.numeric
--- mod_mixte.R
+++ mod_mixte.R
@@ -1,20 +1,18 @@
 library(sommer)
-data_final_pair = data_final[c(1:253)*2,]
-data_final_impair = data_final[c((1:253)*2 -1),]
+data_final_pair = data_final[c(1:270)*2,]
+data_final_impair = data_final[c((1:270)*2 -1),]
 data_final_pair$voisin = data_final_impair$plante_genotype
 data_final_impair$voisin = data_final_pair$plante_genotype
 data_final = as.data.frame(rbind(data_final_pair,data_final_impair))
 
 
-nrow(data_final)
-data_final$Colonnef = as.factor(data_final)
-data_final = data_final[-c(457),]
 data_final$plante_genotype = as.factor(data_final$plante_genotype)
-Model1 = mmer(Biomasse_seche ~ Bloc + Ligne,
+Model1 = mmer(Biomasse_seche ~ Bloc + poids_graine,
               random = ~ plante_genotype,
               data = data_final)
-Model2 = mmer(Biomasse_seche ~ Bloc + Ligne,
-             random = ~ plante_genotype + voisin + voisin:plante_genotype,data = data_final)
+Model2 = mmer(Biomasse_seche ~ Bloc + poids_graine,
+             random = ~ plante_genotype + voisin:plante_genotype,
+             data = data_final)
 
 
 anova.mmer(Model1,Model2)
